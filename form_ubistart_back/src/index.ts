b7909f4cfdce940913cf18c379775ec8a082c134
Fefealfonsi <fefealfonsi@gmail.com>
2025-02-20 11:51:19 -0300

    apagando logs e comentários

--- form_ubistart_back/src/index.ts
+++ form_ubistart_back/src/index.ts
@@ -16,56 +16,6 @@ app.get("/user", controller.getUser)
 app.post("/user", controller.createUser)
 app.put("/user/:id", controller.updateUser)
 
-// ___________________________________________________________
-
-// app.put('/user/:id', async (req: Request, res: Response) => {
-
-//     try {
-
-//         //CONTROLLER
-
-
-//         const idToEdit = req.params.id
-
-//         const newName = req.body.name as string | undefined
-//         const newEmail = req.body.email as string | undefined
-//         const newCep = req.body.cep as string | undefined
-
-
-
-//         const [user] = await db.select("*").from("user").where({ id: idToEdit })
-//         if (user) {
-
-
-//             await db.update({
-//                 name: newName || user.name,
-//                 email: newEmail || user.email,
-//                 user_cep: newCep || user.user_cep
-//             }).from("user").where({ id: idToEdit })
-
-//         } else {
-//             res.status(404)
-//             throw new Error("'id' não encontrada")
-//         }
-
-        
-
-//         res.status(200).send("Atualização realizada com sucesso")
-//     } catch (error: any) {
-//         console.log(error)
-
-//         if (res.statusCode === 200) {
-//             res.status(500)
-//         }
-//         if (error instanceof Error) {
-//             res.send(error.message)
-//         } else {
-//             res.send("Erro inesperado")
-//         }
-//     }
-
-// })
-
 
 
 
